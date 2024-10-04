354941a6bc8094d5d34a4a1ade8d225ff0fb1a55
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2024-10-04 14:47:14 +0200

    MAssa saker

---
+++ Menu.cs
@@ -0,0 +1,168 @@
+namespace GodisAutomaten
+{
+    public static class Menu
+    {
+        //Metod som kan användas av alla andra menyer för att minska Redundans.
+        //Används för att kunna göra val i menyn via Piltangenter, Enter och Backspace
+        public static int ShowMenu(string[] options)
+        {
+            int menuChoice = 0;
+            bool runMenu = true;
+
+            while (runMenu)
+            {
+                Console.Clear();
+                Console.CursorVisible = false;
+
+                for (int i = 0; i < options.Length; i++)
+                {                    
+                    if (i == menuChoice)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;    //För att HighLighta nuvarande menyval med pil och textfärg
+                        Console.WriteLine(options[i] + " <--");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine(options[i]);  //Skriv ut resterande menyval utan pil och highlight
+                    }
+                }
+
+                var keyPressed = Console.ReadKey();
+
+                if (keyPressed.Key == ConsoleKey.DownArrow && menuChoice < options.Length - 1)  //Om menyVal inte redan är på sista index , gå fram ett element
+                {
+                    menuChoice++;
+                }
+                else if (keyPressed.Key == ConsoleKey.UpArrow && menuChoice > 0)    //Om menyVal inte redan är på index 0, gå tillbaka ett element
+                {
+                    menuChoice--;
+                }
+                else if (keyPressed.Key == ConsoleKey.Enter)    //Välj nuvarande index
+                {
+                    return menuChoice;
+                }
+                else if (keyPressed.Key == ConsoleKey.Backspace)    //Vid tryck på "Backspace", välj sista menyvalet(Avsluta/Gå Tillbaka)
+                {
+                    return options.Length - 1;
+                }
+            }
+            return -1;
+        }
+        // public static int ShowMenu(Godis[] options)
+        // {
+        //     int menuChoice = 0;
+        //     bool runMenu = true;
+
+        //     while (runMenu)
+        //     {
+        //         Console.Clear();
+        //         Console.CursorVisible = false;
+
+        //         for (int i = 0; i < options.Length; i++)
+        //         {
+        //             if (i == menuChoice)
+        //             {
+        //                 Console.ForegroundColor = ConsoleColor.DarkCyan;    //För att HighLighta nuvarande menyval med pil och textfärg
+        //                 Console.WriteLine($"LUCKA NR: {i + 1}.   {options[i].Typ} ANTAL: {options[i].Antal} <--");
+        //                 Console.ResetColor();
+        //             }
+        //             else
+        //             {
+        //                 Console.WriteLine($"LUCKA NR: {i + 1}.   {options[i].Typ} ANTAL: {options[i].Antal}");  //Skriv ut resterande menyval utan pil och highlight
+        //             }
+        //         }
+
+        //         var keyPressed = Console.ReadKey();
+
+        //         if (keyPressed.Key == ConsoleKey.DownArrow && menuChoice < options.Length - 1)  //Om menyVal inte redan är på sista index , gå fram ett element
+        //         {
+        //             menuChoice++;
+        //         }
+        //         else if (keyPressed.Key == ConsoleKey.UpArrow && menuChoice > 0)    //Om menyVal inte redan är på index 0, gå tillbaka ett element
+        //         {
+        //             menuChoice--;
+        //         }
+        //         else if (keyPressed.Key == ConsoleKey.Enter)    //Välj nuvarande index
+        //         {
+        //             return menuChoice;
+        //         }
+        //         else if (keyPressed.Key == ConsoleKey.Backspace)    //Vid tryck på "Backspace", välj sista menyvalet(Avsluta/Gå Tillbaka)
+        //         {
+        //             return options.Length - 1;
+        //         }
+        //     }
+        //     return -1;
+        // }
+
+        //Meny-metoder som anropar olika metoder baserat på menyval, anropar först ShowMenu() för själva gränssnittet.
+        public static void MainMenu()
+        {
+            bool runMenu = true;
+            while (runMenu)
+            {
+                //Lägg till så många menyval du vill i menuChoice-Array
+                int menuChoice = ShowMenu(new string[] { "1. Köp godis", "2. Logga in som Admin", "3. Avsluta" });
+                switch (menuChoice)
+                {
+                    case 0:
+                        GodisAutomatHandler.PrintCandy();
+                        //Lägg till metodanrop
+                        break;
+                    case 1:
+                        GodisAutomatHandler.AdminLogin();
+                        //Lägg till metodanrop
+                        break;
+                    case 2:
+                        //Avsluta meny
+                        runMenu = false;
+                        break;
+                }
+            }
+        }
+        // public static void CandyMenu()
+        // {
+        //     bool runMenu = true;
+        //     while (runMenu)
+        //     {
+        //         //Lägg till så många menyval du vill i menuChoice-Array
+        //         int menuChoice = ShowMenu(GodisAutomatHandler.godisAutomat);
+        //         switch (menuChoice)
+        //         {
+        //             case 0:
+        //                 //Lägg till metodanrop
+        //                 break;
+        //             case 1:
+        //                 //Lägg till metodanrop
+        //                 break;
+        //             case 2:
+        //                 //Avsluta meny
+        //                 runMenu = false;
+        //                 break;
+        //         }
+        //     }
+        // }
+
+        //Bekräfta Avsluta
+        public static void ExitMenu()
+        {
+            bool runMenu = true; 
+            while (runMenu)
+            {                
+                Console.WriteLine("Är du säker på att du vill avsluta programmet?\n");
+                int menuChoice = ShowMenu(new string[] { "Ja, Avsluta", "Nej, Gå tillbaka" });
+                switch (menuChoice)
+                {
+                    case 0:
+                        runMenu = false;    //Bekräfta och Avsluta program 
+                        break;
+
+                    case 1:
+                        MainMenu();     //Avbryt och återgå till Main-Menu
+                        break;
+                }
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GodisAutomaten
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace GodisAutomaten
 {
     internal class Program
     {
@@ -6,128 +8,221 @@
         {
             //Metod för att ta bort Godis.Antal vid menyval?
             //If/else statement, om Godis.Antal > 0, gå till Metod, annars, skriv ut att Godis.Typ är slut för tillfället.
-
-            Godis[] godisAutomat = new Godis[5];    //Array av Godis-klass
-
             Godis Japp = new Godis { Typ = "Japp", Antal = 5 };
             Godis Daim = new Godis { Typ = "Daim", Antal = 5 };
             Godis Kola = new Godis { Typ = "Kola", Antal = 5 };
             Godis Lakrits = new Godis { Typ = "Lakrits", Antal = 3 };
             Godis Marabou = new Godis { Typ = "Marabou", Antal = 8 };
 
-            godisAutomat[0] = Japp;
-            godisAutomat[1] = Daim;
-            godisAutomat[2] = Kola;
-            godisAutomat[3] = Lakrits;
-            godisAutomat[4] = Marabou;
+            GodisAutomatHandler.godisAutomat[0] = Japp;
+            GodisAutomatHandler.godisAutomat[1] = Daim;
+            GodisAutomatHandler.godisAutomat[2] = Kola;
+            GodisAutomatHandler.godisAutomat[3] = Lakrits;
+            GodisAutomatHandler.godisAutomat[4] = Marabou;
 
-            bool runMenu = true;
+            Menu.MainMenu();    //Anropa Main-Menu
+            Menu.ExitMenu();    //Vid avslut av Main-Menu, Anropa Exit-Menu för att bekräfta
 
-            while (runMenu) //Menyval
+            //Om Exit-Menu bekräftar avsluta, skriv ut avskedsmeddelande och avsluta programmet
+            Console.Clear();
+            Console.WriteLine("Välkommen åter!");
+            Console.WriteLine("Tryck på valfri tangent för att avsluta programmet...");
+            Console.ReadKey();
+
+        }
+    }
+
+    static class GodisAutomatHandler
+    {
+        public static Godis[] godisAutomat = new Godis[5];    //Array av Godis-klass
+
+        public static void PrintCandy()
+        {
+            while (true)
             {
-                Console.Clear();    //Varje gång loop börjar om(efter varje menyval), rensa skärmen.
-                for (int i = 0; i < godisAutomat.Length; i++)   //Skriv ut varje Godis ur array på ny rad
-                {
-                    Console.WriteLine($"LUCKA NR: {i + 1}.   {godisAutomat[i].Typ} ANTAL: {godisAutomat[i].Antal}");
-                }
-                Console.Write("(För att avsluta, tryck [q]), Annars välj luckans nr: ");
-                string menyVal = Console.ReadLine();
-                Console.WriteLine("");
+                string[] candyOptions = new string[godisAutomat.Length +1];
 
-                switch (menyVal)
+                for (int i = 0; i < godisAutomat.Length; i++)
                 {
-                    case "1":
-                        if (godisAutomat[0].Antal > 0)  //Kolla om luckan är tom eller inte. Om luckan inte är tom, gör detta
-                        {
-                            Console.WriteLine("Du får en Japp. Mums!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            godisAutomat[0].Antal--;    //Ta bort 1 från Antal vid val
-                            Console.ReadKey();  //För att man ska hinna läsa output innan case avslutas.
-                            break;
-                        }
-                        else    //Om luckan är tom, gör detta
-                        {
-                            Console.WriteLine("Tillfälligt slut, prova en annan lucka!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            Console.ReadKey();
-                            break;
-                        }    
-                    case "2":
-                        if (godisAutomat[1].Antal > 0)
-                        {
-                            Console.WriteLine("Du får en Daim. Mums!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            godisAutomat[1].Antal--;
-                            Console.ReadKey();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Tillfälligt slut, prova en annan lucka!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            Console.ReadKey();
-                            break;
-                        }                       
-                    case "3":
-                        if (godisAutomat[2].Antal > 0)
-                        {
-                            Console.WriteLine("Du får en Kola. Mums!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            godisAutomat[2].Antal--;
-                            Console.ReadKey();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Tillfälligt slut, prova en annan lucka!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            Console.ReadKey();
-                            break;
-                        }                       
-                    case "4":
-                        if (godisAutomat[3].Antal > 0)
-                        {
-                            Console.WriteLine("Du får en Lakrits. Mums!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            godisAutomat[3].Antal--;
-                            Console.ReadKey();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Tillfälligt slut, prova en annan lucka!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            Console.ReadKey();
-                            break;
-                        }
-                    case "5":
-                        if (godisAutomat[4].Antal > 0)
-                        {
-                            Console.WriteLine("Du får en Marabou. Mums!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            godisAutomat[4].Antal--;
-                            Console.ReadKey();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Tillfälligt slut, prova en annan lucka!");
-                            Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                            Console.ReadKey();
-                            break;
-                        }    
-                    case "q":   //Vid val att avsluta, sätt runMenu till false för att avsluta while-loop och avsluta program
-                        Console.WriteLine("Välkommen åter!");
-                        Console.ReadKey();
-                        runMenu = false;
+                    candyOptions[i] = $"Lucka: {i + 1}. {godisAutomat[i].ToString()}";
+                }
+                candyOptions[candyOptions.Length - 1] = "Gå tillbaka";
+
+                int choice = Menu.ShowMenu(candyOptions);
+                if (choice == candyOptions.Length -1)
+                {
+                    break;
+                }
+                else
+                {
+                    BuyCandy(choice);
+                    continue;
+                }
+            }
+        }
+        public static void BuyCandy(int i)
+        {
+            if (godisAutomat[i].Antal > 0)
+            {
+                godisAutomat[i].Antal--;
+                Console.WriteLine($"Du köpte en {godisAutomat[i].Typ}. Mums!");
+            }
+            else
+            {
+                Console.WriteLine($"{godisAutomat[i].Typ} är för tillfället slut.");
+            }
+            Console.ReadKey();
+        }
+
+        public static void AdminLogin()
+        {
+            Admin.NewAdmin();
+            bool verify = Admin.VerifyAdmin();
+            if (verify)
+            {
+                AdminHandler();
+            }
+        }
+        public static void AdminHandler()
+        {
+            bool runMenu = true;
+            while (runMenu)
+            {
+                //Lägg till så många menyval du vill i menuChoice-Array
+                int menuChoice = Menu.ShowMenu(new string[] { "1. Fyll på godis", "2. Byt godis", "3. Byt Användarnamn och Lösenord", "4. Gå tillbaka" });
+                switch (menuChoice)
+                {
+                    case 0:
+                        AddCandyMenu();
                         break;
-                    default:    //Vid input som inte finns som ett alternativ i menyn
-                        Console.WriteLine("Fel input!");
-                        Console.WriteLine("Tryck valfri tangen för att fortsätta köpa godis!");
-                        Console.ReadKey();
+                    case 1:
+                        ChangeCandyMenu();
+                        break;
+                    case 2:
+                        Admin.ChangeCredentials();
+                        break;
+                    case 3:
+                        //Avsluta meny
+                        runMenu = false;
                         break;
                 }
             }
+
+        }
+        private static void AddCandyMenu()
+        {
+            while (true)
+            {
+                string[] candyOptions = new string[godisAutomat.Length +1];
+
+                for (int i = 0; i < godisAutomat.Length; i++)
+                {
+                    candyOptions[i] = $"Lucka: {i + 1}. {godisAutomat[i].ToString()}";
+                }
+                candyOptions[candyOptions.Length - 1] = "Gå tillbaka";
+
+                int choice = Menu.ShowMenu(candyOptions);
+                if (choice == candyOptions.Length -1)
+                {
+                    break;
+                }
+                else
+                {
+                    AddCandy(choice);
+                    continue;
+                }
+            }
+        }
+        private static void AddCandy(int i)
+        {
+            Console.WriteLine($"Hur många {godisAutomat[i].Typ} vill du fylla på med?");
+            int antal = int.Parse(Console.ReadLine());
+            if (antal > 0)
+            {
+                godisAutomat[i].Antal += antal;
+            }
+        }
+        private static void ChangeCandyMenu()
+        {
+            while (true)
+            {
+                string[] candyOptions = new string[godisAutomat.Length +1];
+
+                for (int i = 0; i < godisAutomat.Length; i++)
+                {
+                    candyOptions[i] = $"Lucka: {i + 1}. {godisAutomat[i].ToString()}";
+                }
+                candyOptions[candyOptions.Length - 1] = "Gå tillbaka";
+
+                Console.WriteLine("Vilken lucka vill du byta godis ur?");
+                int choice = Menu.ShowMenu(candyOptions);
+                if (choice == candyOptions.Length -1)
+                {
+                    break;
+                }
+                else
+                {
+                    ChangeCandy(choice);
+                    continue;
+                }
+            }
+        }
+        private static void ChangeCandy(int i)
+        {
+            Console.Write("Vad vill du lägga till för ny godistyp: ");
+            string nyGodisTyp = Console.ReadLine();
+            Console.Write("Hur många vill du lägga till: ");
+            int antal = int.Parse(Console.ReadLine());
+
+            Godis nyGodis = new Godis { Typ = nyGodisTyp, Antal = antal };
+            godisAutomat[i] = nyGodis;
+        }
+
+    }
+
+    class Admin
+    {
+        public string username;
+        public string password;
+        public static Admin[] admin = new Admin[1];
+        public Admin(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        public static void NewAdmin()
+        {
+            if (admin[0] == null)
+            {
+                Admin newAdmin = new Admin("admin", "admin");
+                admin[0] = newAdmin;
+            }
+        }
+        public static bool VerifyAdmin()
+        {
+            Console.Write("Skriv in användarnamn: ");
+            string username = Console.ReadLine();
+            Console.Write("Skriv in lösenord: ");
+            string password = Console.ReadLine();
+            if (admin[0].username == username && admin[0].password == password)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Fel användarnamn eller lösenord");
+                Console.ReadKey();
+                return false;
+            }
+        }
+        public static void ChangeCredentials()
+        {
+                Console.Write("Skriv in nytt användarnamn: ");
+                string username = Console.ReadLine();
+                Console.Write("Skriv in nytt lösenord: ");
+                string password = Console.ReadLine();
+                Admin updateAdmin = new Admin(username, password);
+                admin[0] = updateAdmin;
         }
     }
 
@@ -135,5 +230,11 @@
     {
         public string Typ { get; set; }
         public int Antal { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Typ, -15}  Antal: {Antal}";
+        }
     }
 }
+
